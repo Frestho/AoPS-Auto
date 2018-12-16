959af6ffb4f3c35e9f42501d07ec9fc46f7a25f8
e or <34761944+bleakly@users.noreply.github.com>
2018-12-15 18:40:58 -0800

    Add files via upload

---
+++ Homework saving script.py
@@ -0,0 +1,19 @@
+import keyboard, time
+week = 3
+time.sleep(5)
+while week < 8:
+    keyboard.press_and_release('ctrl+l')
+    time.sleep(0.1) #wait for browser to respond
+    keyboard.write('https://artofproblemsolving.com/class/1809-maa-amc10/homework/' + str(week))#go to homework page
+    keyboard.press_and_release('enter')
+    time.sleep(5) #wait for page to fully load (5 seconds)
+    keyboard.press_and_release('ctrl+p')
+    time.sleep(1) #wait for printing menu to pop up (1 second)
+    keyboard.press_and_release('enter')
+    time.sleep(5) #wait for printing job to finish (10 seconds)
+    keyboard.write('Week ' + str(week) + ' HW')
+    keyboard.press_and_release('enter')
+    time.sleep(0.5) #wait for file to get saved
+    week += 1
+    
+    
