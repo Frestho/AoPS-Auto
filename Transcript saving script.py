2b82dd394ae93a980210014af33785ee82c2b037
e or <34761944+bleakly@users.noreply.github.com>
2018-12-16 20:07:05 -0800

    number tweaks

--- Transcript saving script.py
+++ Transcript saving script.py
@@ -6,11 +6,11 @@ while week < 8:
     time.sleep(0.1) #wait for browser to respond
     keyboard.write('https://artofproblemsolving.com/class/1809-maa-amc10/transcript/' + str(25244 + week))#go to homework page
     keyboard.press_and_release('enter')
-    time.sleep(8) #wait for page to fully load (5 seconds)
+    time.sleep(8) #wait for page to fully load (8 seconds)
     keyboard.press_and_release('ctrl+p')
     time.sleep(1) #wait for printing menu to pop up (1 second)
     keyboard.press_and_release('enter')
-    time.sleep(14) #wait for printing job to finish (12 seconds)
+    time.sleep(14) #wait for printing job to finish (14 seconds)
     keyboard.write('Week ' + str(week) + ' Class')
     keyboard.press_and_release('enter')
     time.sleep(0.5) #wait for file to get saved
