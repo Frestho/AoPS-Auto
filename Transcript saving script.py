8eb19abada364deaec5cfc72acb1aa97c8dbe460
e or <34761944+bleakly@users.noreply.github.com>
2019-08-12 14:05:58 -0700

    Improved instructions

--- Transcript saving script.py
+++ Transcript saving script.py
@@ -4,7 +4,16 @@ time.sleep(5)
 while week < 25:
     keyboard.press_and_release('ctrl+l')
     time.sleep(0.1) #wait for browser to respond
-    keyboard.write('https://artofproblemsolving.com/class/1809-maa-amc10/transcript/' + str(25244 + week))#go to homework page
+    
+    
+    
+    #Go to your week 1 transcript and paste it in the url below, replacing the existing one. Then move the number at the end here:
+    #                          |             |                                               |   
+    #                          v             v                                               v 
+    keyboard.write('https://artofproblemsolving.com/class/1809-maa-amc10/transcript/' + str(25244 + week-1))#go to homework page
+    
+    
+    
     keyboard.press_and_release('enter')
     time.sleep(8) #wait for page to fully load (8 seconds)
     keyboard.press_and_release('ctrl+p')
